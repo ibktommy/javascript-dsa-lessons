a1680a83799ddb1760103efe6db680cf1a15a245
ibktommy <atomquartz@gmail.com>
2023-09-24 23:35:52 +0100

    Testing the titleCase-function with a different input

--- 01-basic-challenges-part1/06-title-case/title-case-run.js
+++ 01-basic-challenges-part1/06-title-case/title-case-run.js
@@ -2,6 +2,8 @@ const titleCase = require('./title-case')
 
 const output1 = titleCase('tHe');
 const output2 = titleCase('HERE IS MY HANDLE HERE IS MY SPOUT')
+const output3 = titleCase("I'm a little tea pot")
 
 console.log(output1);
 console.log(output2);
+console.log(output3);
