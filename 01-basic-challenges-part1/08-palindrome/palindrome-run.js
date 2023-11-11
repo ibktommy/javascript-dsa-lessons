24666be263df62995cad272a3448265164a2a47b
ibktommy <atomquartz@gmail.com>
2023-11-11 20:21:09 +0100

    Create function to get the character codes in a string

--- 01-basic-challenges-part1/08-palindrome/palindrome-run.js
+++ 01-basic-challenges-part1/08-palindrome/palindrome-run.js
@@ -1,7 +1,12 @@
-const palindrome = require('./palindrome.js')
+// const palindrome = require('./palindrome.js')
+const getCharCode = require('./palindrome.js');
 
 const output1 = 'woman'
 const output2 = 'madam'
+const output3= 'a'
 
-console.log(palindrome(output1));
-console.log(palindrome(output2));
+// console.log(palindrome(output1));
+// console.log(palindrome(output2));
+console.log(getCharCode(output1));
+console.log(getCharCode(output2));
+console.log(getCharCode(output3));
--- 01-basic-challenges-part1/08-palindrome/palindrome.js
+++ 01-basic-challenges-part1/08-palindrome/palindrome.js
@@ -1,18 +1,28 @@
 // Function that takes in a string and check if it is a palindrome, i.e when the string is reversed the word is also same as the original
 
-function palindrome(word) {
-  let reversedWord = ''
+// function palindrome(word) {
+//   let reversedWord = ''
 
-  for (let i = word.length - 1; i >= 0; i--) {
-    reversedWord += word[i]
-  }
+//   for (let i = word.length - 1; i >= 0; i--) {
+//     reversedWord += word[i]
+//   }
 
-  // return reversedWord
-  if (reversedWord === word) {
-    return `"${word}" is an example of a Palindrome`
-  } else {
-    return `"${word}" is not an example of a Palindrome`;
+//   // return reversedWord
+//   if (reversedWord === word) {
+//     return `"${word}" is an example of a Palindrome`
+//   } else {
+//     return `"${word}" is not an example of a Palindrome`;
+//   }
+// }
+
+// Function to get the character codes in a string
+function getCharCode(str) {
+  for (let i = 0; i <= str.length - 1; i++) {
+   console.log(str.charCodeAt(i));
   }
 }
 
-module.exports = palindrome
+// module.exports = palindrome
+module.exports = getCharCode
+
+
